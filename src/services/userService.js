862d5bef553f4f76a77c599cf619c760cf024c70
Tolulope <tjcode19@gmail.com>
2023-06-06 22:38:40 +0100

    feat:Verify Email

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
-// src/services/userService.js
-
 const UserRepository = require("../repositories/userRepo");
+const CR = require("../utils/customResponses");
+const CU = require("../utils/utils");
 
 class UserService {
   constructor() {
@@ -33,6 +33,72 @@ class UserService {
 
   async checkOtpExist(otp, userId) {
     return await this.userRepository.getUserByQuery({ otp: otp, _id: userId });
+  }
+
+  async verifyEmail(otp, password, userId) {
+    try {
+      const otpExist = await this.userService.checkOtpExist(otp, userId);
+      if (otpExist) {
+        const saltR = 10;
+        const hashPass = await bcrypt.hash(password, saltR);
+        let data = {
+          username: otpExist.email,
+          password: hashPass,
+          salt: saltR,
+          _id: userId,
+        };
+        const newAuth = await this.authService.createAuth(data);
+        if (newAuth) {
+          this.userService.updateUser(userId, { otp: "" });
+          const t = CU.generateAccessToken({
+            userId: userId,
+            type: "User",
+          });
+          return {
+            status: 201,
+            res: {
+              code: CR.accepted,
+              message: "Request Successful",
+              data: {
+                token: t,
+              },
+            },
+          };
+        } else {
+          return {
+            status: 500,
+            res: {
+              code: CR.serverError,
+              message: "Request Failed",
+            },
+          };
+        }
+      } else {
+        return {
+          status: 400,
+          res: {
+            code: CR.badRequest,
+            message: "Invalid OTP",
+          },
+        };
+      }
+    } catch (error) {
+      if (String(error).includes("MongoNotConnectedError")) {
+        return {
+          status: 500,
+          res: { code: CR.serverError, message: "Database connection error" },
+        };
+      }
+
+      return {
+        status: 500,
+        res: {
+          code: CR.serverError,
+          message: "Internal server error:" + error,
+          dev: "In Login AuthService",
+        },
+      };
+    }
   }
 }
 
