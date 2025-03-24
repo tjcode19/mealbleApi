801c190042d07e45996bf59c7356e96216037a8f
Tolulope <tjcode19@gmail.com>
2025-03-24 13:18:33 +0100

    ammend

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ app.use("/timetable", timetableRouter);
 app.use("/subscription", subscriptionRouter);
 app.use("/store", storeRouter);
 app.use("/notification", notificationRouter);
-app.use("google-rtdn");
 
 // app.options("*", cors());
 // app.use(express.static("./public"));
